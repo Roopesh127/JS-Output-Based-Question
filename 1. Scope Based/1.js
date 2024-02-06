46313f885c356088ffa439d1c233da526eaeb540
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-02-06 20:29:26 +0530

    06/02

--- 1. Scope Based/1.js
+++ 1. Scope Based/1.js
@@ -1,5 +1,68 @@
-{
-    let a = 5;
-    console.log(a) // 5
-}
-console.log(a)  // a is not fined
+//  {
+//     let a = 5;
+//     console.log(a) // 5
+// }
+// console.log(a)  // a is not fined
+
+
+
+
+// let friend = "roopesh";
+
+// function getfriend(){
+//     console.log(friend); 
+
+//     let enemy = "Amit";
+//     return function getenemy(){
+//         console.log(enemy)  // 
+//     };
+    
+// }
+
+
+
+// function sayHi(){
+//     console.log(name);  // undefined
+//     console.log(age);  // going to temporal dead zone and refrence error
+//     var name  = "ayush";
+//     let age = 22;
+// }
+// sayHi()
+
+
+
+// function getage(){
+//     'use strict';
+//     age = 21;
+//     console.log(age); // refrence - age is not defined beacause inside strict mode we can not declare any accidentally variable.
+// }
+// getage()
+
+
+// +true          // 1 , beacause by type cohersion it convert true = 1.
+// !"ayush"       // flase , beacause filled string is always truthy value.
+
+
+// let number = 0;
+// console.log(number++);     // 0 , there is value 0 but after this value is incremented.
+// console.log(++number);     // 2 , after incremented 1 and this is pre postpone then 1+1 = 2.
+// console.log(number);       // 2
+
+
+
+// String.prototype.giveAyushPizza = () =>{
+//     return 'just give Ayush pizza alredy !';
+// };
+// const name = 'Roopesh';
+// name.giveAyushPizza();
+
+
+
+// function sayHi(){
+//     return (()=>0)();
+// }
+// console.log(sayHi())   // 0
+// console.log(typeof sayHi());
+
+
+
---
+++ 2. IFFE Based/1.js
@@ -0,0 +1,76 @@
+// var result = (function(x){
+//     delete x;
+//     return x;
+// })(0);
+// console.log(result)
+
+
+
+// var x = 10;
+// (function (){
+//     console.log(x);
+// })();
+
+
+
+// var x = 10;
+// (function(){
+//     var x= 20;
+//     console.log(x);
+// })();
+
+
+
+// var x = 10;
+// (function(){
+//     var x = 20;
+// })();
+// console.log(x);
+
+
+
+// var x = 10;
+// (function(){
+//     x = 20;
+// })();
+// console.log(x)
+
+
+// var x = 10;
+// (function(){
+//     delete x;
+// })();
+// console.log(x)
+
+
+// (function(x){
+//     console.log(x);
+// })(5);
+
+
+// (function(){
+//     console.log(x);
+//     var x = 5;
+// })();
+
+
+
+// (function(){
+//     console.log(x);
+//     x = 5;
+// })();
+// var x =10;
+
+
+
+// var x = (function(){
+//     return 20;
+// })();
+// console.log(x);
+
+
+
+// var x = (function() {
+//     return 20;
+// }());              // new way to define iife
+// console.log(x);
