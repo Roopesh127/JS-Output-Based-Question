ddc83f347f5155ec2b7aeee01f1b23d10a1cc7ce
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-02-15 01:14:17 +0530

    15/02

--- 6. Type Cohersion based Question/1.js
+++ 6. Type Cohersion based Question/1.js
@@ -24,3 +24,158 @@
 
 
 
+// let result = "3" + 2
+// console.log(result)
+
+
+// let result = 3 + true;
+// console.log(result)
+
+
+
+// let x = 3 + undefined
+// console.log(x)
+
+
+
+// let y = 3 + null;
+// console.log(y)
+
+
+
+// let result = "4" - "2"
+// console.log(result)
+
+
+// let x = "4" - 2
+// console.log(x)
+
+
+
+// let x = "8" * 2
+// console.log(x)
+
+
+// let result = "hello" - "world";
+// console.log(result)
+
+
+
+// let x = "4" - "hello"
+// console.log(x)
+
+
+
+// let x = "4" - true
+// console.log(x)
+
+
+// let x = "4" - 1
+// console.log(x)
+
+
+
+// let x = 4 + true
+// console.log(x)
+
+
+
+// let x = 4 + false;
+// console.log(x)
+
+
+
+// let x = 4 + null;
+// console.log(x)
+
+
+
+// let x = 4 -null;
+// console.log(x)
+
+
+
+// let x = 4 + undefined;
+// console.log(x)
+
+
+
+// let x = 4 - undefined;
+// console.log(x)
+
+
+
+// let x = null + undefined;
+// console.log(x)
+
+
+
+// let x = Number("324e-1")
+// console.log(x)
+
+
+
+// let x = Number(true);
+// console.log(x)
+
+
+
+// let x = Number(null)
+// console.log(x)
+
+
+
+// let x = Number(" ")
+// console.log(x)
+
+
+// let x = Number("hdc")
+// console.log(x)
+
+
+
+// let x = Number(undefined)
+// console.log(x)
+
+
+
+// let x = parseInt("233.45")
+// console.log(x)
+
+
+// let x = parseFloat("2.3443.4")
+// console.log(x)
+
+
+// let x = +"23.433"
+// console.log(x)
+
+
+
+// let x = +"23.4578"
+// console.log(x)
+
+
+
+// let x = Math.floor("20.13434")
+// console.log(x)
+
+
+
+// let x = String(345)
+// console.log(x)
+
+
+
+// let x = String(2+4);
+// console.log(x)
+
+
+
+// let x = String(null)
+// console.log(x)
+
+
+
+let x = Number("345e-1")
+console.log(x)
